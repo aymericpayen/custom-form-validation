6da62292c2f3ef2eb92e4f7ff8bade2743cadb93
Dorian Climb <dorianclimb@Dorians-MacBook-Air.local>
2022-09-27 08:59:16 +0200

    remove_questionnaire_related_parts

--- app/models/questionnaire.rb
+++
@@ -1,7 +0,0 @@
-class Questionnaire < ApplicationRecord
-  validates :reference, presence: true
-  validates :reference, length: {minimum: 2}
-  validates :reference, uniqueness: true
-  validates :content, presence: true
-  validates_with QuestionnaireValidator
-end
